11208dcb3c91a0425dac4589bfbf36b6415a5513
Chadrack Mbuyi Kalala <chadrackmbuyi2@gmail.com>
2021-03-12 06:42:32 +0200

    First commit

---
+++ src/za/ac/cput/team1/Game.java
@@ -0,0 +1,13 @@
+package za.ac.cput.team1;
+
+public class Game {
+
+    public static void main(String[] args) {
+
+        String a = "Chad";
+        String b = "Smith";
+        String together;
+        together = a + b;
+        System.out.println(together);
+    }
+}
